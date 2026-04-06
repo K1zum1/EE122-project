60fa4afba024973608c44e861cbc083ea154886f
Jay <jaychong1203@gmail.com>
2026-04-05 19:29:23 -0700

    basic topology (still working on)

---
+++ src/topology.py
@@ -0,0 +1,34 @@
+from mininet.net import Mininet
+from mininet.node import Controller, OVSKernelSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel
+
+def car_network():
+    net = Mininet(controller=Controller, switch=OVSKernelSwitch)
+
+    print("***SDN Controller") #basic controller
+    net.addController('c0')
+
+    print("***Central Switch") # virtual switch
+    s1 = net.addSwitch('s1')
+
+    print("***ECUs")
+    infotainment = net.addHost('attacker', ip='10.0.0.1') #virtual host
+    brakes = net.addHost('victim', ip='10.0.0.2') # virtual machine as the victim
+
+    print("***Car")
+    net.addLink(infotainment, s1) #infotainment to central switch
+    net.addLink(brakes, s1) # brake nodes to central switch
+
+    print("***Starting the vehicle network") #boot network
+    net.start()
+
+    print("***Type 'pingall' to test.") #network is live
+    CLI(net)
+
+    print("***Shutting down") #shut down after
+    net.stop()
+
+if __name__ == '__main__': #boilerplate
+    setLogLevel('info')
+    car_network
